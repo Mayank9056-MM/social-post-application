d1cb3ca539102b8121c4ce7e94e90131cec59395
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-02-02 22:34:22 +0530

    feat: add authentication middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import User from '../models/user.model.js';
+import { ApiError } from '../utils/ApiError.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import jwt from 'jsonwebtoken';
+
+export const verifyAuth = asyncHandler(async (req, res, next) => {
+    const accessToken =
+        req.cookies.accessToken ||
+        req.header('Authorization')?.replace('Bearer ', ''); // if sent from header/mobile
+
+    if (!accessToken) {
+        throw new ApiError(401, 'Access token not found');
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            accessToken,
+            process.env.ACCESS_TOKEN_SECRET
+        );
+
+        const user = await User.findById(decodedToken?._id).select('-password');
+
+        if (!user) {
+            throw new ApiError(401, 'Unauthorized');
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || 'Invalid access token');
+    }
+});
