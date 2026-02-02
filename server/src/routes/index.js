7ee07097cc27918bff27b451fe20cdbd10c31414
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-02-02 22:34:36 +0530

    feat: add post routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
-import express from 'express'
-import healthRouter from './healthcheck.routes.js'
-import userRouter from './user.routes.js'
+import express from 'express';
+import healthRouter from './healthcheck.routes.js';
+import userRouter from './user.routes.js';
+import postRouter from './post.routes.js';
 
 const router = express.Router();
 
 router.use('/health', healthRouter);
 router.use('/users', userRouter);
+router.use('/posts', postRouter);
 
 export default router;
---
+++ server/src/routes/post.routes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {
+    addComment,
+    createPost,
+    deletePost,
+    getAllPosts,
+    toggleLike,
+    updatePost,
+} from '../controllers/post.controllers.js';
+import { upload } from '../middlewares/multer.middleware.js';
+import { verifyAuth } from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+router.post('/create', upload.single('image'), createPost);
+router.patch('/update/:postId', verifyAuth, updatePost);
+router.delete('/delete/:postId', verifyAuth, deletePost);
+router.get('/all-posts', verifyAuth, getAllPosts);
+router.post('/add-comment/:postId', verifyAuth, addComment);
+router.post('toggle-like', verifyAuth, toggleLike);
+
+export default router;
